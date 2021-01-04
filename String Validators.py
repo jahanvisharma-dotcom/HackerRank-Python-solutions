08a31c204420c1f1f98dfab3d7307003ef488c24
Jahanvi Sharma <56484763+jahanvisharma-dotcom@users.noreply.github.com>
2021-01-04 21:19:09 +0530

    Create String Validators.py

---
+++ String Validators.py
@@ -0,0 +1,19 @@
+string = raw_input()
+l=list(string)
+a,b,c,d,e=False,False,False,False,False
+for i in l:
+    if i.isalnum():
+        a=True
+    if i.isalpha():
+        b=True
+    if i.isdigit():
+        c=True
+    if i.islower():
+        d=True
+    if i.isupper():
+        e=True
+print a
+print b
+print c
+print d
+print e
